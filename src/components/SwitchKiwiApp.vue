99b9f3f76a22d0704f0e1b7774f4badba422b359
Troy <1045132927@qq.com>
2025-06-05 00:38:20 +0800

    ✨ feat: add inline app name editing and update header title

--- src/components/SwitchKiwiApp.vue
+++ src/components/SwitchKiwiApp.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { KIWI_APP_RECENT } from '@/lib/storageKeys'
-import { ArrowRightLeft } from 'lucide-vue-next'
-import { type PropType } from 'vue'
+import { ArrowRightLeft, Check, Pencil } from 'lucide-vue-next'
+import { nextTick, ref, useTemplateRef, type PropType } from 'vue'
 import { useRouter } from 'vue-router'
 
 defineProps({
@@ -10,22 +10,73 @@ defineProps({
     required: true,
   },
 })
+
 const router = useRouter()
+const isEditMode = ref(false)
+const $input = useTemplateRef<HTMLInputElement>('$input')
 
 function handleSwitchApp() {
   localStorage.removeItem(KIWI_APP_RECENT)
   router.replace('/')
 }
+
+async function handleEditApp() {
+  isEditMode.value = true
+  await nextTick()
+  $input.value?.focus()
+}
+function handleConfirmEdit() {
+  isEditMode.value = false
+}
 </script>
 
 <template>
-  <a-button
+  <div
     v-if="appInfo"
-    class="w-full !justify-between gap-2 !py-4 !bg-blue-50/50 !rounded-md"
-    type="outline"
-    @click="handleSwitchApp"
+    class="flex justify-between items-center gap-2 p-2 pl-4 bg-blue-50/50 rounded-md border border-blue-600 text-blue-600"
   >
-    <span class="font-medium">{{ appInfo!.name }}</span>
-    <ArrowRightLeft :size="14" />
-  </a-button>
+    <span v-if="!isEditMode" class="font-medium">{{ appInfo!.name }}</span>
+    <input
+      v-else
+      ref="$input"
+      class="flex-1 w-0 outline-0"
+      placeholder="App Name"
+      v-model="appInfo!.name"
+    />
+
+    <div v-if="!isEditMode">
+      <a-button
+        size="mini"
+        type="text"
+        class="hover:!bg-blue-100/50"
+        @click="handleEditApp"
+      >
+        <template #icon>
+          <Pencil :size="14" />
+        </template>
+      </a-button>
+
+      <a-button
+        size="mini"
+        type="text"
+        class="hover:!bg-blue-100/50"
+        @click="handleSwitchApp"
+      >
+        <template #icon>
+          <ArrowRightLeft :size="14" />
+        </template>
+      </a-button>
+    </div>
+    <a-button
+      v-else
+      size="mini"
+      type="text"
+      class="hover:!bg-blue-100/50"
+      @click="handleConfirmEdit"
+    >
+      <template #icon>
+        <Check :size="14" />
+      </template>
+    </a-button>
+  </div>
 </template>
