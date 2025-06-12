99f0d93fa7f3c96e55372717548148fcb13d2fdd
Troy <1045132927@qq.com>
2025-06-12 16:30:55 +0800

    ✨ feat: add parameter editor and support method invocation with params

--- src/pages/[appId]/[qualifiedName]/_components/MethodInvoker.vue
+++ src/pages/[appId]/[qualifiedName]/_components/MethodInvoker.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
 import { useKiwiAppAndSchemaStore } from '@/stores/useKiwiAppAndSchemaStore'
-import { computed, ref, watch } from 'vue'
+import { computed, ref, useTemplateRef, watch } from 'vue'
+import ParamterEditor from './ParamterEditor/Index.vue'
 
 const kiwiAppAndSchemaStore = useKiwiAppAndSchemaStore()
 const targetMethod = computed(() => kiwiAppAndSchemaStore.willInvokeMethod)
 const hasParameters = computed(() => !!targetMethod.value?.parameters.length)
 const visible = ref(false)
+const $parameterEditor =
+  useTemplateRef<InstanceType<typeof ParamterEditor>>('$parameterEditor')
+
 watch(targetMethod, () => {
   visible.value = !!targetMethod.value
 })
 
 function handleCloseModal() {
   visible.value = false
+}
+
+async function handleExcuteMethod() {
+  const objectId = kiwiAppAndSchemaStore.selectedObject?.__id__
+  const methodName = targetMethod.value?.name
+  if (!objectId || !methodName) return false
+  const parameters = $parameterEditor.value?.getParameters()
+  try {
+    await kiwiAppAndSchemaStore
+      .getKiwiSchema()
+      ?.invokeMethod(objectId, methodName, parameters)
+    return true
+  } catch (error) {
+    return false
+  }
 }
 </script>
 
@@ -23,6 +42,7 @@ function handleCloseModal() {
     :mask-closable="false"
     :simple="!hasParameters"
     ok-text="Excute"
+    :on-before-ok="handleExcuteMethod"
     @ok="handleCloseModal"
     @cancel="handleCloseModal"
     @close="kiwiAppAndSchemaStore.invokeMethod(null)"
@@ -32,6 +52,12 @@ function handleCloseModal() {
     </template>
     <div v-if="!hasParameters" class="text-center mb-6">
       This action cannot be undo.
+    </div>
+    <div v-else>
+      <ParamterEditor
+        ref="$parameterEditor"
+        :parameters="targetMethod!.parameters"
+      />
     </div>
   </a-modal>
 </template>
