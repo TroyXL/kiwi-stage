729273062701574ea7dd956229311eb39648dded
Troy <1045132927@qq.com>
2025-06-13 20:43:33 +0800

    ✨ feat: add form validation and required flag for parameters

--- src/pages/[appId]/[qualifiedName]/_components/parameterEditor/Index.vue
+++ src/pages/[appId]/[qualifiedName]/_components/parameterEditor/Index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { KiwiParameter } from '@/kiwi/schema/parameter'
 import type { KiwiPrimitiveType } from '@/kiwi/schema/type'
+import type { FormInstance } from '@arco-design/web-vue'
+import type { FieldRule } from '@arco-design/web-vue/es/form/interface'
 import { ref } from 'vue'
 import PrimitiveParameterEditor from './PrimitiveParameterEditor.vue'
 
@@ -9,18 +11,33 @@ const props = defineProps<{
   model?: Dict
 }>()
 
-const formModel = ref<Dict>(
+const formModel = ref(
   props.parameters.reduce((acc, cur) => {
     acc[cur.name] = props.model?.[cur.name]
     return acc
   }, {} as Dict)
 )
 
+const formRules = ref(
+  props.parameters.reduce((acc, cur) => {
+    acc[cur.name] = {
+      required: cur.required,
+      message: `${cur.label || cur.name} is required`,
+    }
+    return acc
+  }, {} as Dict<FieldRule>)
+)
+
+const $form = ref<FormInstance>()
+
 function handleParameterChanged(value: any, name: string) {
   formModel.value[name] = value
 }
 
-function getParameters() {
+async function getParameters() {
+  const errors = await $form.value?.validate()
+  if (errors) return
+
   return formModel.value
 }
 
@@ -30,9 +47,18 @@ defineExpose({
 </script>
 
 <template>
-  <a-form :model="formModel" :wrapper-col-props="{ span: 16 }">
+  <a-form
+    ref="$form"
+    :model="formModel"
+    :rules="formRules"
+    :wrapper-col-props="{ span: 16 }"
+  >
     <template v-for="parameter in parameters">
-      <a-form-item v-if="!parameter.ignore" :label="parameter.label">
+      <a-form-item
+        v-if="!parameter.ignore"
+        :label="parameter.label"
+        :field="parameter.name"
+      >
         <PrimitiveParameterEditor
           v-if="parameter.type.kind === 'primitive'"
           :name="parameter.name"
--- src/pages/[appId]/[qualifiedName]/_components/parameterEditor/PrimitiveParameterEditor.vue
+++ src/pages/[appId]/[qualifiedName]/_components/parameterEditor/PrimitiveParameterEditor.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import type { KiwiPrimitiveType } from '@/kiwi/schema/type'
+import { isNil } from 'lodash'
 import { ref, watch } from 'vue'
 
 const FLOAT_TYPES: KiwiPrimitiveTypeEnum[] = ['long', 'float', 'double']
+const TYPE_DEFAULT_VALUES = {
+  isInteger: 0,
+  isFloat: 0,
+  isBoolean: false,
+  isString: '',
+}
 
 const props = defineProps<{
   name: string
@@ -13,15 +20,47 @@ const emits = defineEmits<{
   changed: [value: any, name: string]
 }>()
 
-const model = ref(props.value)
-watch(model, () => {
-  emits('changed', model.value, props.name)
-})
+const typeAsserts = (() => {
+  const typeName = props.type.name
+  const isInteger = typeName === 'int'
+  const isFloat = FLOAT_TYPES.includes(typeName)
+  const isBoolean = typeName === 'boolean'
+  const isString = typeName === 'string'
+
+  const defaultValue = isInteger
+    ? TYPE_DEFAULT_VALUES.isInteger
+    : isFloat
+    ? TYPE_DEFAULT_VALUES.isFloat
+    : isBoolean
+    ? TYPE_DEFAULT_VALUES.isBoolean
+    : isString
+    ? TYPE_DEFAULT_VALUES.isString
+    : void 0
+
+  return {
+    isInteger,
+    isFloat,
+    isBoolean,
+    isString,
+    defaultValue,
+  }
+})()
+
+const model = ref(props.value ?? typeAsserts.defaultValue)
+watch(
+  model,
+  () => {
+    emits('changed', model.value, props.name)
+  },
+  {
+    immediate: isNil(props.value),
+  }
+)
 </script>
 
 <template>
   <a-input-number
-    v-if="type.name === 'int'"
+    v-if="typeAsserts.isInteger"
     v-model="model"
     placeholder="Please Enter"
     mode="button"
@@ -29,18 +68,18 @@ watch(model, () => {
     :precision="0"
   />
   <a-input-number
-    v-if="FLOAT_TYPES.includes(type.name)"
+    v-else-if="typeAsserts.isFloat"
     v-model="model"
     placeholder="Please Enter"
     mode="button"
   />
   <a-switch
-    v-if="type.name === 'boolean'"
+    v-else-if="typeAsserts.isBoolean"
     placeholder="Please Enter"
     v-model="model"
   />
   <a-input
-    v-if="type.name === 'string'"
+    v-else-if="typeAsserts.isString"
     placeholder="Please Enter"
     v-model.trim="model"
   />
