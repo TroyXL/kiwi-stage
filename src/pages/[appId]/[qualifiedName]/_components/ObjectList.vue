30ad4f447edc9a57200f1ff4ed09dc6bb57a47af
Troy <1045132927@qq.com>
2025-06-15 23:13:31 +0800

    ✨ refactor: extract object list into reusable component for clarity

---
+++ src/pages/[appId]/[qualifiedName]/_components/ObjectList.vue
@@ -0,0 +1,150 @@
+<script setup lang="ts">
+import { KiwiApp } from '@/kiwi'
+import type { KiwiTableRow } from '@/kiwi/schema/field'
+import { useEmitter } from '@/lib/emitter'
+import text from '@/lib/text'
+import Scaffold from '@/pages/_components/Scaffold.vue'
+import type { TableColumnData } from '@arco-design/web-vue'
+import { usePagination } from 'alova/client'
+import { computed } from 'vue'
+
+const ACTION_COLUMN: TableColumnData = {
+  dataIndex: '__actions__',
+  title: '',
+  align: 'right',
+  fixed: 'right',
+  width: 60,
+  slotName: 'actions',
+}
+
+const props = defineProps<{
+  qualifiedName: string
+  isSelectMode?: boolean
+}>()
+
+const emit = defineEmits<{
+  showDetail: [id: string]
+  select: [row: KiwiTableRow]
+}>()
+
+const kiwiSchema = computed(() =>
+  KiwiApp.current?.getSchemaByQualifiedName(props.qualifiedName)
+)
+
+const rowSelection = props.isSelectMode
+  ? { type: 'radio', fixed: true, width: 40 }
+  : void 0
+
+const columns = computed<TableColumnData[]>(() => {
+  if (!kiwiSchema.value) return []
+  const tableColumns = kiwiSchema.value.tableColumns.map(column => ({
+    ...column,
+    fixed: column.field.summary ? 'left' : void 0,
+    minWidth: 200,
+    // width: column.field.summary ? void 0 : 200,
+  }))
+  return props.isSelectMode ? tableColumns : tableColumns.concat(ACTION_COLUMN)
+})
+
+const {
+  loading,
+  data,
+  page,
+  pageSize,
+  total,
+  reload: handleResetObjectList,
+  send: handleRefreshObjectList,
+} = usePagination(
+  (page, pageSize) =>
+    KiwiApp.current!.fetchObjects({
+      page,
+      pageSize,
+      type: props.qualifiedName,
+    }),
+  {
+    total: response => response.total,
+    data: response =>
+      kiwiSchema.value?.transformObjectsToTableRows(response.items) ?? [],
+    initialData: {
+      total: 0,
+      items: [],
+    },
+    initialPage: 1,
+    initialPageSize: 10,
+    watchingStates: [() => props.qualifiedName],
+  }
+)
+
+function handleSelectRow(_: any, __: any, record: KiwiTableRow) {
+  emit('select', record)
+}
+
+useEmitter('refreshObjectList', handleRefreshObjectList)
+</script>
+
+<template>
+  <Scaffold
+    bodyClass="flex flex-col !p-0"
+    :title="kiwiSchema?.label"
+    :subtitle="qualifiedName"
+  >
+    <template #actions>
+      <a-space>
+        <a-button type="primary">
+          <template #icon>
+            <icon-plus />
+          </template>
+          <span>{{ text.createLabel }}</span>
+        </a-button>
+
+        <template v-if="isSelectMode">
+          <span>Select Mode</span>
+        </template>
+        <template v-else>
+          <a-button @click="handleResetObjectList">
+            <template #icon>
+              <icon-refresh />
+            </template>
+            <span>{{ text.refreshLabel }}</span>
+          </a-button>
+        </template>
+      </a-space>
+    </template>
+
+    <div class="h-0 flex-1 p-8 overflow-auto">
+      <a-table
+        stripe
+        row-key="__id__"
+        class="rounded-none"
+        :loading="loading"
+        :columns="columns"
+        :data="data"
+        :pagination="false"
+        :sticky-header="-32"
+        :row-selection="rowSelection"
+        @select="handleSelectRow"
+      >
+        <template #actions="{ record }">
+          <a-button
+            type="text"
+            class="hover:!bg-blue-100/80"
+            @click="emit('showDetail', record.__id__)"
+          >
+            <template #icon>
+              <icon-file />
+            </template>
+          </a-button>
+        </template>
+      </a-table>
+    </div>
+
+    <div class="flex justify-end px-8 py-4 border-t">
+      <a-pagination
+        v-model:current="page"
+        :page-size="pageSize"
+        :total="total"
+        show-total
+      />
+    </div>
+  </Scaffold>
+</template>
--- src/pages/[appId]/[qualifiedName]/index.vue
+++ src/pages/[appId]/[qualifiedName]/index.vue
@@ -1,78 +1,18 @@
 <script setup lang="ts">
-import { KiwiApp } from '@/kiwi'
-import { useEmitter } from '@/lib/emitter'
-import text from '@/lib/text'
-import Scaffold from '@/pages/_components/Scaffold.vue'
 import {
   useKiwiAppAndSchemaStore,
   useKiwiSchemaByRouteAndGetParams,
 } from '@/stores/useKiwiAppAndSchemaStore'
-import { TableColumnData } from '@arco-design/web-vue'
-import { usePagination } from 'alova/client'
-import { computed } from 'vue'
 import { useRouter } from 'vue-router'
+import ObjectList from './_components/ObjectList.vue'
 
 defineOptions({
   name: 'KiwiObjectsPage',
 })
 
-const ACTION_COLUMN: TableColumnData = {
-  dataIndex: '__actions__',
-  title: '',
-  align: 'right',
-  fixed: 'right',
-  width: 60,
-  slotName: 'actions',
-}
-
 const router = useRouter()
 const params = useKiwiSchemaByRouteAndGetParams()
 const kiwiAppAndSchemaStore = useKiwiAppAndSchemaStore()
-const kiwiSchema = computed(() => kiwiAppAndSchemaStore.kiwiSchema)
-const qualifiedName = computed(() => kiwiAppAndSchemaStore.qualifiedName)
-
-const columns = computed<TableColumnData[]>(() => {
-  if (!kiwiSchema.value) return []
-  return kiwiSchema.value.tableColumns
-    .map(column => ({
-      ...column,
-      fixed: column.field.summary ? 'left' : void 0,
-      minWidth: 200,
-      // width: column.field.summary ? void 0 : 200,
-    }))
-    .concat(ACTION_COLUMN)
-})
-
-const {
-  loading,
-  data,
-  page,
-  pageSize,
-  total,
-  reload: handleResetObjectList,
-  send: handleRefreshObjectList,
-} = usePagination(
-  (page, pageSize) =>
-    KiwiApp.current!.fetchObjects({
-      page,
-      pageSize,
-      type: qualifiedName.value,
-    }),
-  {
-    total: response => response.total,
-    data: response =>
-      kiwiAppAndSchemaStore.kiwiSchema?.transformObjectsToTableRows(
-        response.items
-      ) ?? [],
-    initialData: {
-      total: 0,
-      items: [],
-    },
-    initialPage: 1,
-    initialPageSize: 10,
-    watchingStates: [qualifiedName],
-  }
-)
 
 function handleShowObjectDetail(objectId: string) {
   router.replace({
@@ -83,71 +23,11 @@ function handleShowObjectDetail(objectId: string) {
     },
   })
 }
-
-function handlePageChange(nextPage: number) {
-  page.value = nextPage
-}
-
-useEmitter('refreshObjectList', handleRefreshObjectList)
 </script>
 
 <template>
-  <Scaffold
-    bodyClass="flex flex-col !p-0"
-    :title="kiwiSchema?.label"
-    :subtitle="qualifiedName"
-  >
-    <template #actions>
-      <a-space>
-        <a-button type="primary">
-          <template #icon>
-            <icon-plus />
-          </template>
-          <span>{{ text.createLabel }}</span>
-        </a-button>
-
-        <a-button @click="handleResetObjectList">
-          <template #icon>
-            <icon-refresh />
-          </template>
-          <span>{{ text.refreshLabel }}</span>
-        </a-button>
-      </a-space>
-    </template>
-
-    <div class="h-0 flex-1 p-8 overflow-auto">
-      <a-table
-        stripe
-        row-key="__id__"
-        class="rounded-none"
-        :loading="loading"
-        :columns="columns"
-        :data="data"
-        :pagination="false"
-        :sticky-header="-32"
-        @page-change="handlePageChange"
-      >
-        <template #actions="{ record }">
-          <a-button
-            type="text"
-            class="hover:!bg-blue-100/80"
-            @click="handleShowObjectDetail(record.__id__)"
-          >
-            <template #icon>
-              <icon-file />
-            </template>
-          </a-button>
-        </template>
-      </a-table>
-    </div>
-
-    <div class="flex justify-end px-8 py-4 border-t">
-      <a-pagination
-        v-model:current="page"
-        :page-size="pageSize"
-        :total="total"
-        show-total
-      />
-    </div>
-  </Scaffold>
+  <ObjectList
+    :qualified-name="kiwiAppAndSchemaStore.qualifiedName"
+    @show-detail="handleShowObjectDetail"
+  />
 </template>
