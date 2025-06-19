3bcc638059ac1f1ae1ed909fb70c47a679658642
Troy <1045132927@qq.com>
2025-06-19 17:20:40 +0800

    ✨ refactor: replace ParameterEditor with ObjectEditor for object editing

--- src/pages/[appId]/[qualifiedName]/_components/objectEditor/ObjectEditor.vue
+++ src/pages/[appId]/[qualifiedName]/_components/objectEditor/ObjectEditor.vue
@@ -1,80 +1,49 @@
 <script setup lang="ts">
 import type { KiwiSchema } from '@/kiwi'
-import { i18nKey, useI18nText } from '@/lib/i18n'
-import { useKiwiAppAndSchemaStore } from '@/stores/useKiwiAppAndSchemaStore'
-import { Message } from '@arco-design/web-vue'
-import { computed, useTemplateRef } from 'vue'
+import { useI18nText } from '@/lib/i18n'
+import { useTemplateRef } from 'vue'
 import ParameterEditor from '../parameterEditor/Index.vue'
 
 const props = defineProps<{
-  targetSchema: KiwiSchema
+  schema: KiwiSchema
   data?: KiwiObject
-}>()
-const visible = defineModel<boolean>('visible')
-const emit = defineEmits<{
-  refresh: [newObjectId?: string]
-  close: []
+  asChild?: boolean
 }>()
 
 const t = useI18nText()
 const $parameterEditor =
   useTemplateRef<InstanceType<typeof ParameterEditor>>('$parameterEditor')
-const { kiwiApp } = useKiwiAppAndSchemaStore()
-const hasParameters = computed(() => {
-  if (props.targetSchema?.constructorParameters.length) return true
-})
+const hasParameters = !!props.schema?.constructorParameters.length
 
-function handleCloseModal() {
-  visible.value = false
-}
-
-async function handleConfirmEdit() {
-  if (!kiwiApp) return false
+async function getObjectPaylod() {
   const errors = await $parameterEditor.value?.validate()
-  if (errors) return false
+  if (errors) return
   const parameters = $parameterEditor.value?.getParameters()
-  const payload: KiwiCreateOrUpdateObject = {
-    object: {
-      id: props.data?.id,
-      type: props.targetSchema.qualifiedName,
-      fields: parameters,
-    },
-  }
-  try {
-    const newObjectId = await kiwiApp?.createOrUpdateObject(payload)
-    emit('refresh', newObjectId)
-    Message.success(t(i18nKey.successTip))
-    return true
-  } catch (error) {
-    return false
-  }
+
+  if (!props.asChild)
+    return {
+      object: {
+        id: props.data?.id,
+        type: props.schema.qualifiedName,
+        fields: parameters,
+      },
+    } as KiwiCreateOrUpdateObject
+
+  return {
+    [props.schema.qualifiedName]: [{ fields: parameters }],
+  } as KiwiCreateOrUpdateObjectChildren
 }
+
+defineExpose({
+  getObjectPaylod,
+})
 </script>
 
 <template>
-  <a-modal
-    unmount-on-close
-    v-model:visible="visible"
-    :title-align="hasParameters ? 'start' : void 0"
-    :closable="false"
-    :mask-closable="false"
-    :simple="!hasParameters"
-    :ok-text="$t(i18nKey.submitLabel)"
-    :on-before-ok="handleConfirmEdit"
-    @ok="handleCloseModal"
-    @cancel="handleCloseModal"
-    @close="emit('close')"
-  >
-    <template #title>{{
-      $t(data?.id ? i18nKey.editRecordTitle : i18nKey.createRecordTitle)
-    }}</template>
-    <div v-if="!hasParameters">{{ $t(i18nKey.actionUndoTip) }}</div>
-    <div v-else>
-      <ParameterEditor
-        ref="$parameterEditor"
-        :parameters="targetSchema!.constructorParameters"
-        :data="data"
-      />
-    </div>
-  </a-modal>
+  <ParameterEditor
+    v-if="hasParameters"
+    ref="$parameterEditor"
+    :parameters="schema!.constructorParameters"
+    :data="data"
+  />
 </template>
