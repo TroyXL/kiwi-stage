3a7e85071d3d733d2e539aa631b179f03a22c160
Troy <1045132927@qq.com>
2025-07-05 07:37:19 +0800

    feat(登录): 添加生产环境下的特殊登录方式
    在生产环境中添加双击logo或点击特定字符序列的隐藏登录方式

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { KiwiManager } from '@/kiwi'
+import { IS_PRODUCTION } from '@/lib/constants'
 import { i18nKey } from '@/lib/i18n'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
@@ -7,15 +8,46 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 const loading = ref(false)
 
+const targetClickContent = 'WELCOMETOKIWI'.split('').reverse().join('')
+const welcomeChars = 'Welcome to KIWI'.split('')
+
 async function handleLogin() {
   loading.value = true
   await KiwiManager.shared.loginAsDemo()
   router.replace('/')
 }
+
+let clickedChars: string[] = []
+function handleLoginByDbClickLogo() {
+  const clickedContent = clickedChars.join('').toUpperCase()
+  clickedChars = []
+  if (clickedContent === targetClickContent) handleLogin()
+}
+function handleClickChar(char: string) {
+  clickedChars.push(char)
+}
 </script>
 
 <template>
+  <div v-if="IS_PRODUCTION" class="fixed-center flex flex-col items-center">
+    <img
+      src="/logo.png"
+      alt="Kiwi Logo"
+      class="size-24"
+      @dblclick="handleLoginByDbClickLogo"
+    />
+    <p class="text-3xl font-medium">
+      <span
+        v-for="(char, index) in welcomeChars"
+        :key="index"
+        class="px-1"
+        @click="handleClickChar(char)"
+        >{{ char }}</span
+      >
+    </p>
+  </div>
   <a-button
+    v-else
     class="fixed-center"
     type="primary"
     :loading="loading"
