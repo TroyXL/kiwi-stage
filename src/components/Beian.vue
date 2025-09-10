3370230e8f5b162293300362fb34dd864c8beaf8
Troy <1045132927@qq.com>
2025-09-10 09:17:06 +0800

    feat(页面布局): 添加备案信息组件并调整侧边栏样式
    在侧边栏底部添加备案信息组件，同时调整侧边栏菜单的内边距使其更美观

---
+++ src/components/Beian.vue
@@ -0,0 +1,9 @@
+<template>
+  <a
+    class="block w-full text-center text-muted-foreground text-xs hover:text-[rgb(var(--primary-6))] hover:underline"
+    href="https://beian.miit.gov.cn/"
+    target="_blank"
+  >
+    苏ICP备2022038935号-1
+  </a>
+</template>
